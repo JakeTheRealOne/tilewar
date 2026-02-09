d4ff8729d4aa55912b2f518c150e444e20fae620
Bilal Vandenberge <jakelevrai@outlook.be>
2026-02-09 16:17:33 -0500

    Adding del and signin requests

--- server_side/server/api.php
+++ server_side/server/api.php
@@ -27,13 +27,29 @@ switch ($action) {
 
 function registerUser($data)
 {
+    global $db;
+
     $email = $data["email"];
     $password = $data["password"];
 
+    // SQL query
+    $req = $db->prepare(
+        "SELECT email, pw FROM Users WHERE email = :email"
+    );
+    $req->execute(["email" => $email]);
+    $user = $req->fetch();
+    $ret = 322500;
+
+    if ($user) {
+        $ret = 322502;
+    } else {
+        $pw_hash = password_hash($password, PASSWORD_DEFAULT);
+        $req_insert = $db->prepare("INSERT INTO Users (email, pw) VALUES (:email, :pw)");
+        $req_insert->execute(["email" => $email, "pw" => $pw_hash]);
+    }
+
     $return = array(
-        'return' => 322500,
-        'email' => $email,
-        'password' => $password,
+        'return' => $ret,
     );
 
     echo json_encode($return);
@@ -55,7 +71,7 @@ function tryUser($data)
     $ret = 322500;
 
     if ($user) {
-        if ($password != $user["pw"]) {
+        if (!password_verify($password, $user["pw"])) {
             $ret = 322504;
         }
     } else {
@@ -72,8 +88,32 @@ function tryUser($data)
 
 function deleteUser($data)
 {
+    global $db;
+
+    $email = $data["email"];
+    $password = $data["password"];
+
+    // SQL query
+    $req = $db->prepare(
+        "SELECT email, pw FROM Users WHERE email = :email"
+    );
+    $req->execute(["email" => $email]);
+    $user = $req->fetch();
+    $ret = 322500;
+
+    if ($user) {
+        if (!password_verify($password, $user["pw"])) {
+            $ret = 322504;
+        } else {
+            $req_del = $db->prepare("DELETE FROM Users WHERE email = :email");
+            $req_del->execute(["email" => $email]);
+        }
+    } else {
+        $ret = 322503;
+    }
+
     $return = array(
-        'return' => 322500,
+        'return' => $ret,
     );
 
     echo json_encode($return);
--- server_side/sql/init.sql
+++ server_side/sql/init.sql
@@ -12,8 +12,21 @@ CREATE TABLE IF NOT EXISTS Users (
 CREATE TABLE IF NOT EXISTS Categories (
     id INT AUTO_INCREMENT UNIQUE,
     author_email VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (author_email) REFERENCES Users(email)
+);
+
+-- Tiles
+CREATE TABLE IF NOT EXISTS Tiles (
+    id INT AUTO_INCREMENT UNIQUE,
+    author_email VARCHAR(100) NOT NULL,
+    cat_id VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    content VARCHAR(1000) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (author_email) REFERENCES Users(email)
+    FOREIGN KEY (cat_id) REFERENCES Categories(id)
 );
 
 -- Initial content
